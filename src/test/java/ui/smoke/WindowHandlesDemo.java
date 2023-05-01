45f84fae7c6e31a74724e265d981c6dd44b1ef8d
burulai-narzieva <burulainarzieva2@gmail.com>
2023-05-01 12:21:04 +0300

    added new tests

--- src/test/java/ui/smoke/WindowHandlesDemo.java
+++ src/test/java/ui/smoke/WindowHandlesDemo.java
@@ -4,10 +4,11 @@ import com.digital.ui.driver.BrowserHelper;
 import com.digital.ui.driver.Driver;
 import com.digital.ui.element_helper.WebElementActions;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class WindowHandlesDemo {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
 
 
         BrowserHelper browserHelper = new BrowserHelper();
@@ -16,7 +17,6 @@ public class WindowHandlesDemo {
         Driver.getDriver().findElement(By.id("tabButton")).click();
         Driver.getDriver().findElement(By.id("tabButton")).click();
         Driver.getDriver().findElement(By.id("tabButton")).click();
-
         WebElementActions.pause(2000);
         browserHelper.switchToWindow(1);
         WebElementActions.pause(2000);
