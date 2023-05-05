1e9f59fab58992ac81b2b66939be089c0756611a
Kubanych <ktashbaev1989@gmail.com>
2023-05-05 13:23:59 +0600

    smth changed

--- src/main/java/com/digital/ui/pages/BasePage.java
+++ src/main/java/com/digital/ui/pages/BasePage.java
@@ -1,5 +1,6 @@
 package com.digital.ui.pages;
 
+import com.digital.ui.driver.Driver;
 import com.digital.ui.driver.DriverActions;
 import com.digital.ui.element_helper.WebElementActions;
 import org.openqa.selenium.support.PageFactory;
