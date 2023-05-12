25ed2ffea72aab832214d7c8a1e05fd89af3dfa8
unknown <kasmaliveargen@gmail.com>
2023-05-12 16:43:52 +0600

    Написал  автоматизацию на страницу Users Group

--- src/main/java/com/digital/utils/WaitforSeconds.java
+++ src/main/java/com/digital/utils/WaitforSeconds.java
@@ -1,2 +1,11 @@
-package com.digital.utils;public class WaitforSeconds {
+package com.digital.utils;
+
+public class WaitforSeconds {
+    public static void IfYouNeedTowait(int seconds){
+        try{
+            Thread.sleep(seconds*1000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
 }
